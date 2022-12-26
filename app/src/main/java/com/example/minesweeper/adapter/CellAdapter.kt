d33ee2b8a8cdcc774ecded4bba89f5f65ab6b0ad
Adam Petrovic <adampetrox@gmail.com>
2022-12-25 22:15:29 -0800

    Refactored cell image calculation

--- app/src/main/java/com/example/minesweeper/adapter/CellAdapter.kt
+++ app/src/main/java/com/example/minesweeper/adapter/CellAdapter.kt
@@ -5,52 +5,29 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.recyclerview.widget.RecyclerView
-import com.example.minesweeper.MainActivity
 import com.example.minesweeper.R
+import com.example.minesweeper.domain.BoardCell
 import com.example.minesweeper.domain.GameBoard
 
 @Suppress("IMPLICIT_CAST_TO_ANY")
 class CellAdapter(
-    private val context: MainActivity,
     private val gameBoard: GameBoard,
 ) : RecyclerView.Adapter<CellAdapter.ItemViewHolder>() {
 
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val cell : ImageButton = view.findViewById(R.id.cellButton)
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CellAdapter.ItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val adapterLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.view_cell, parent, false)
 
         return ItemViewHolder(adapterLayout)
     }
 
-    override fun onBindViewHolder(holder: CellAdapter.ItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val cellBackground = if (position % 2 == 1) R.drawable.tile1 else R.drawable.tile2
         val cellState = gameBoard.cellAt(position)
-
-        val cellImage = if (cellState.isRevealed) {
-            if (cellState.hasBomb) {
-                R.drawable.explosion
-            } else {
-                when(cellState.minesInArea) {
-                    0 -> R.drawable.zero
-                    1 -> R.drawable.one
-                    2 -> R.drawable.two
-                    3 -> R.drawable.three
-                    4 -> R.drawable.four
-                    5 -> R.drawable.five
-                    6 -> R.drawable.six
-                    else -> cellBackground
-                }
-            }
-        } else {
-            if (cellState.isFlagged) {
-                R.drawable.flag
-            } else {
-                cellBackground
-            }
-        }
+        val cellImage = cellState.toCellDrawable(cellBackground)
 
         holder.cell.setImageResource(cellImage)
 
@@ -65,6 +42,30 @@ class CellAdapter(
 
     override fun getItemCount() = gameBoard.cellCount()
 
+    private fun BoardCell.toCellDrawable(cellBackground: Int): Int {
+        return if (isRevealed) {
+            if (hasBomb) {
+                R.drawable.explosion
+            } else {
+                when(minesInArea) {
+                    0 -> R.drawable.zero
+                    1 -> R.drawable.one
+                    2 -> R.drawable.two
+                    3 -> R.drawable.three
+                    4 -> R.drawable.four
+                    5 -> R.drawable.five
+                    6 -> R.drawable.six
+                    else -> cellBackground
+                }
+            }
+        } else {
+            if (isFlagged) {
+                R.drawable.flag
+            } else {
+                cellBackground
+            }
+        }
+    }
     private fun cellOnLongClick(position: Int): Boolean {
         gameBoard.placeFlagAt(position)
         notifyDataSetChanged()
@@ -75,6 +76,4 @@ class CellAdapter(
         notifyDataSetChanged()
         return true
     }
-
-
 }
